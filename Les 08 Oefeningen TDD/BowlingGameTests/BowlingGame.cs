59500e2a63cb1327eaf1ac048c512fc71cd06796
Gijs Verbist <s114092@ap.be>
2020-11-10 20:32:59 +0100

    Bowling Game: stap 2

--- Les 08 Oefeningen TDD/BowlingGameTests/BowlingGame.cs
+++ Les 08 Oefeningen TDD/BowlingGameTests/BowlingGame.cs
@@ -2,8 +2,13 @@
 {
     internal class BowlingGame
     {
+        public int Score { get; set; }
         public BowlingGame()
         {
+        }
+        public void Roll(int NumberHit)
+        {
+
         }
     }
 }
--- Les 08 Oefeningen TDD/BowlingGameTests/BowlingGameScoreTests.cs
+++ Les 08 Oefeningen TDD/BowlingGameTests/BowlingGameScoreTests.cs
@@ -10,9 +10,17 @@ namespace BowlingGameScoreTests
         }
 
         [Test]
-        public void CanCreateGame()
+        public void When_Roll_GlutterGame_Returns_0()
         {
-            var game = new BowlingGame();
+            var bowlingGame = new BowlingGame();
+            int counter = 0;
+            do
+            {
+                bowlingGame.Roll(0);
+                counter++;
+            } while (counter <= 20);
+
+            Assert.AreEqual(0, bowlingGame.Score);
         }
     }
 }
